c9aa679e1a664b82d044587a041eb18bd99a87a0
Ворошилова Елизавета <liza21012005uk@gmail.com>
2025-05-16 22:56:19 +0700

    Список книг

---
+++ lab6/books.js
@@ -0,0 +1,73 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: 600,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Рэй Брэдбери',
+        name: '451 градус по Фаренгейту',
+        publicationYear: 1953,
+        price: 550,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 450,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 700,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Эрих Мария Ремарк',
+        name: 'Три товарища',
+        publicationYear: 1936,
+        price: 650,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Антуан де Сент-Экзюпери',
+        name: 'Маленький принц',
+        publicationYear: 1943,
+        price: 500,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Джоан Роулинг',
+        name: 'Гарри Поттер и философский камень',
+        publicationYear: 1997,
+        price: 900,
+        bestseller: true,
+        inStock: true
+    }
+];
